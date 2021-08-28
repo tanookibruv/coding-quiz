80b7425404da0a6c26e25cd4e34626b5a50899fb
tanookibruv <erendon1836@gmail.com>
2021-08-27 23:57:47 -0700

    editing a few functions

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var timeEl = document.querySelector("#time");
 var startEl = document.querySelector(".start-quiz");
-var mainPage = document.querySelector(".main")
-var openingPage = document.querySelector(".opening")
-var questionsEl = document.querySelector("#q-and-a")
-var timerEl =  document.querySelector("#time")
+var mainPage = document.querySelector(".main");
+var openingPage = document.querySelector(".opening");
+var leaderEL = document.querySelector("#leaderboard");
+var questionsEl = document.querySelector("#q-and-a");
+var timerEl =  document.querySelector("#time");
 var timerCount = 15;
 
 var questions = [
@@ -39,11 +40,11 @@ var questions = [
       choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
       answer: "console.log"
     }
-  ];
-
+];
 
 function initGame() {
     var openingPage = document.querySelector(".opening");
+    
     openingPage.setAttribute("class", "hide");
 
     questionsEl.removeAttribute("class");
@@ -52,22 +53,25 @@ function initGame() {
 }
 
 function startTimer() {
-    timer = setInterval(function() {
+    var timerInterval = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
 
-        if(timerCount >=0) {
-            if(correct > 0) {
-                resetInterval(timer);
-                nextQuestion()
-            }
-        }
-
-        if (timerCount <= 0) {
-            quizEnd();
+        if (timerCount === 0) {
+          clearInterval(timerInterval);
+        
+          quizEnd();
         }
 
     }, 1000);
+}
+
+function quizEnd() {
+  timerEl.textContent = " ";
+  var leaderEL = document.querySelector("#leaderboard")
+  questionsEl.setAttribute("class", "hide");
+
+  leaderEL.removeAttribute("class");
 }
 
 startEl.onclick = initGame;
