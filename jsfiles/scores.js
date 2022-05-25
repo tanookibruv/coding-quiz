8906a0619d0c1434148495c865bf42a413b73070
tanookibruv <erendon1836@gmail.com>
2022-05-24 21:51:09 -0700

    highscores appear from local storage but are undefined

---
+++ jsfiles/scores.js
@@ -0,0 +1,17 @@
+function printHS () {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + " - " + score.score;
+
+        var olEl = document.getElementById("highscores");
+        olEl.appendChild(liTag);
+    });
+}
+
+printHS();
